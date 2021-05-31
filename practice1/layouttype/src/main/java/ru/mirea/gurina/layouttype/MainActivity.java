6a727bfb723eae4732a0c18b0472b450e993df92
gu3220 <gurinaksusha@list.ru>
2021-05-31 22:12:57 +0300

    last version

---
+++ practice1/layouttype/src/main/java/ru/mirea/gurina/layouttype/MainActivity.java
@@ -0,0 +1,17 @@
+package ru.mirea.gurina.layouttype;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
